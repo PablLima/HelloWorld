450c3d657e5096590616f8fca0c4e20097c51532
Pablo Lodi <lodiricardopablo@gmail.com>
2019-03-11 15:23:09 -0300

    Usa mult e quad na main

--- main.c
+++ main.c
@@ -5,6 +5,10 @@ int main()
 {
 	int r = soma(1, 3);
 	printf("r = %d\n", r);
+	int m = mult(2,2);
+	printf("m = %d\n", m);
+	int q = quad(2);
+	printf("q = %d\n", q);
 	printf("Quer tc?\n");
 	return 0;
 }
